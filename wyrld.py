ff722fad0e55feb5be80c10af8d8e70bdfb08e19
nken <nken@lle.rochester.edu>
2023-02-15 15:17:26 -0500

    show guess

--- wyrld.py
+++ wyrld.py
@@ -9,12 +9,12 @@ def main():
 
     # where are start guessing
     for guess_num in range(1,7):
-        guess = input(f"\nGuess {guess_num}: ").upper();
+        guess = input(f"\nGuess {guess_num}: ").upper(); # verify that the guess will match the all upper current word
 
-        show_guess(...);
-
-        if guess == word:
-            break;
+        show_guess(guess, word);
+        if guess == word: #validate if these are correct.
+            print(f"Correct! the word was {word}")
+            break; #only exit if they guess correctly.
     
     # end game
     else:
@@ -29,28 +29,22 @@ def get_random_word(word_list):
 
     return random.choice(words);
 
-# for guess_num in range(1,7):
-#     guess = input(f"\nGuess a word : ").upper() # verify that the guess will match the all upper current word
-#     if guess == word: # should keep guessing if the word was not correct
-#         print(f"Correct! the word was {word}")
-#         break #only exit if they guess correctly.
+def show_guess(guess, word): #
+    #3 types of options for letters provided in guess
+    # verify letter is valid amd in correct location
+    correct_letters = { 
+        letter for letter, correct in zip(guess, word) if letter == correct
+    } # ZIP: element-by-element comparisons between elements, in our case, the guess and the word.
 
-#     #3 types of options for letters provided in guess
-#     # verify letter is valid amd in correct location
-#     correct_letters = { 
-#         letter for letter, correct in zip(guess, word) if letter == correct
-#     } # ZIP: element-by-element comparisons between elements, in our case, the guess and the word.
+    # correct letters, wrong location
+    misplaced_letters = set(guess) & set(word) - correct_letters # remove the already accounted for letter
 
-#     # correct letters, wrong location
-#     misplaced_letters = set(guess) & set(word) - correct_letters # remove the already accounted for letter
+    # set() and set() provide intersecting elements
 
-#     # set() and set() provide intersecting elements
+    # letters not included in word.
+    wrong_letters = set(guess) - set(word) 
 
-#     # letters not included in word.
-#     wrong_letters = set(guess) - set(word) 
+    print("Correct letters: ",", ".join(sorted(correct_letters)));
+    print("Misplaced letters: ",", ".join(sorted(misplaced_letters)));
+    print("Wrong letters: ",", ".join(sorted(wrong_letters)));
 
-#     print("Correct letters: ",", ".join(sorted(correct_letters)))
-#     print("Misplaced letters: ",", ".join(sorted(misplaced_letters)))
-#     print("Wrong letters: ",", ".join(sorted(wrong_letters)))
-# else:
-#     print(f"The word was {word}")
