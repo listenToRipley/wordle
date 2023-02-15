1d4c88a0b447ee52a14d946a9de3cacac84a93db
nken <nken@lle.rochester.edu>
2023-02-15 17:02:24 -0500

    guess validation

--- wyrld.py
+++ wyrld.py
@@ -6,18 +6,27 @@ def main():
     # find the current word.
     words_path = pathlib.Path(__file__).parent / "wordlist.txt"; #location of file where words are stored.
     word = get_random_word(words_path.read_text(encoding="utf-8").split("\n")); #param is the list of words you want to pick from
+    
+    bad_guess = False;
 
     # where are start guessing - main loop
     for guess_num in range(1,7):
         guess = input(f"\nGuess {guess_num}: ").upper(); # verify that the guess will match the all upper current word
 
-        show_guess(guess, word);
-        if guess == word: #validate if these are correct.
-            break; #only exit if they guess correctly.
+        if len(guess) != 5 and all(letter in ascii_letters for letter in guess):
+            bad_guess = True;
+        
+        if bad_guess:
+            print('Sorry, all guesses must be 5 letters long, please try again.')
+
+        else: 
+            show_guess(guess, word);
+            if guess == word: #validate if these are correct.
+                break; #only exit if they guess correctly.
     
     # end game
     else:
-        game_over(...);
+        game_over(word);
 
 def get_random_word(word_list):
     words = [
@@ -36,12 +45,12 @@ def show_guess(guess, word): #
     } # ZIP: element-by-element comparisons between elements, in our case, the guess and the word.
 
     # correct letters, wrong location
-    misplaced_letters = set(guess) & set(word) - correct_letters # remove the already accounted for letter
+    misplaced_letters = set(guess) & set(word) - correct_letters; # remove the already accounted for letter
 
     # set() and set() provide intersecting elements
 
     # letters not included in word.
-    wrong_letters = set(guess) - set(word) 
+    wrong_letters = set(guess) - set(word);
 
     print("Correct letters: ",", ".join(sorted(correct_letters)));
     print("Misplaced letters: ",", ".join(sorted(misplaced_letters)));
