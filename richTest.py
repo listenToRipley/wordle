40dc595379f48092f563a5b9c8b84ab060437489
listenToRipley <nataliekendrick@Natalies-MacBook-Pro.local>
2023-02-25 16:07:32 -0500

    rich notes

--- richTest.py
+++ richTest.py
@@ -1,2 +1,14 @@
-from rich import print
-print("Hello, [bold red]Rich[/] :snake:")
+# info on Rich 
+# from rich import print;
+
+# print("Hello, [bold red]Rich[/] :snake:");
+
+# See all supported emoji's by running python -m rich.emoji 
+
+# The approve print function is not generally what is used to create a better interaction, use Console since it allows be a better interaction.
+
+from rich.console import Console; 
+
+console = Console();
+
+console.print("Hello, [bold red]Rich[/] :wrapped_gift:");
